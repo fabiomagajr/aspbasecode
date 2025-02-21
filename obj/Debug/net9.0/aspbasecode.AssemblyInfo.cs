29af0e8c7b897e9d8979630532a896fb00ccc35e
fabiomagajr <fabiomagajr@gmail.com>
2025-02-21 15:17:10 -0300

    obj

--- obj/Debug/net9.0/aspbasecode.AssemblyInfo.cs
+++ obj/Debug/net9.0/aspbasecode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspbasecode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fc67ada33058db5bd9b2ecfd8a824df0190301")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09472bbff8ed43ce6c5c5a07a1cfdc90854992a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspbasecode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspbasecode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
